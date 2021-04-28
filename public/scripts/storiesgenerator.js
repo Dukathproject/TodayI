301142519f060e8e172a27cfb8e8b7ea48552b93
Dukathproject <dukathproject@github.com>
2021-04-28 00:27:36 -0300

    Change Fonts

--- public/scripts/storiesgenerator.js
+++ public/scripts/storiesgenerator.js
@@ -8,22 +8,29 @@ fetch('/storiesList').then(res => res.json()).then(function(json) {
 });
 
 //GET ELEMENTS
+var storyGroup = document.getElementById("storyGroup");
 var selectedStory = document.getElementById("selectedStory");
 var bodyText = document.getElementById("bodytext");
 var userName = document.getElementById("name");
 var place = document.getElementById("place");
 var date = document.getElementById("date");
+var randFontList = ["storyFont1", "storyFont2", "storyFont3", "storyFont4", "storyFont5"];
 
 //RANDOM NUMBER
 function randomInt(min, max){
     return min + Math.floor((max - min) * Math.random())
 }
-
-//TIMER
-setTimeout(setInterval(function(){
-    //SELECT RANDOMIZED STORY
+//SELECT RANDOMIZED STORY
+function changeStory(){
+    var randFont = randomInt(0, 4);
     var randNum = randomInt(0, stories.length);
     var storyNum = stories[randNum];
+
+    //CHANGE FONT
+    if(storyGroup.classList != ""){
+        storyGroup.classList.remove(storyGroup.classList);
+    }
+    storyGroup.classList.add(randFontList[randFont]);;
     //UPDATE HTML
     selectedStory.classList.add("animatedTransition");
     // AGREGAR IF "UBICACION VACIA" "NOMBRE ANONYMOUS"
@@ -35,4 +42,9 @@ setTimeout(setInterval(function(){
     };
     place.textContent = storyNum.place;
     date.textContent = storyNum.date;
+}
+
+//TIMER
+setTimeout(setInterval(function(){
+    changeStory();
 }, 8000), 1000);
